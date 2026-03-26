abd2f1e3eef9a1b2f1df4df7147c72b21742c0f3
Nan Zhang <dandange8005@users.noreply.github.com>
2026-03-26 15:23:49 +0000

    fix: update asset filenames to match files on disk

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -23,8 +23,8 @@ export const teams: TeamData[] = [
     logoImage: "redbull.png",
     carImage: "redbull.png",
     drivers: [
-      { number: 1,  firstName: "Max",     lastName: "VERSTAPPEN", nationality: "Dutch",         flag: "🇳🇱", age: 27, image: "verstappen.jpg" },
-      { number: 30, firstName: "Liam",    lastName: "LAWSON",     nationality: "New Zealander", flag: "🇳🇿", age: 23, image: "lawson.jpg" },
+      { number: 1,  firstName: "Max",     lastName: "VERSTAPPEN", nationality: "Dutch",         flag: "🇳🇱", age: 27, image: "max-verstappen.png" },
+      { number: 30, firstName: "Liam",    lastName: "LAWSON",     nationality: "New Zealander", flag: "🇳🇿", age: 23, image: "liam-lawson.png" },
     ],
   },
   {
@@ -33,8 +33,8 @@ export const teams: TeamData[] = [
     logoImage: "ferrari.png",
     carImage: "ferrari.png",
     drivers: [
-      { number: 16, firstName: "Charles", lastName: "LECLERC",  nationality: "Monégasque", flag: "🇲🇨", age: 27, image: "leclerc.jpg" },
-      { number: 44, firstName: "Lewis",   lastName: "HAMILTON", nationality: "British",    flag: "🇬🇧", age: 40, image: "hamilton.jpg" },
+      { number: 16, firstName: "Charles", lastName: "LECLERC",  nationality: "Monégasque", flag: "🇲🇨", age: 27, image: "charles-leclerc.png" },
+      { number: 44, firstName: "Lewis",   lastName: "HAMILTON", nationality: "British",    flag: "🇬🇧", age: 40, image: "lewis-hamilton.png" },
     ],
   },
   {
@@ -43,8 +43,8 @@ export const teams: TeamData[] = [
     logoImage: "mercedes.png",
     carImage: "mercedes.png",
     drivers: [
-      { number: 63, firstName: "George", lastName: "RUSSELL",   nationality: "British", flag: "🇬🇧", age: 27, image: "russell.jpg" },
-      { number: 12, firstName: "Kimi",   lastName: "ANTONELLI", nationality: "Italian", flag: "🇮🇹", age: 18, image: "antonelli.jpg" },
+      { number: 63, firstName: "George", lastName: "RUSSELL",   nationality: "British", flag: "🇬🇧", age: 27, image: "george-russell.png" },
+      { number: 12, firstName: "Kimi",   lastName: "ANTONELLI", nationality: "Italian", flag: "🇮🇹", age: 18, image: "kimi-antonelli.png" },
     ],
   },
   {
@@ -53,8 +53,8 @@ export const teams: TeamData[] = [
     logoImage: "mclaren.png",
     carImage: "mclaren.png",
     drivers: [
-      { number: 4,  firstName: "Lando", lastName: "NORRIS",  nationality: "British",    flag: "🇬🇧", age: 25, image: "norris.jpg" },
-      { number: 81, firstName: "Oscar", lastName: "PIASTRI", nationality: "Australian", flag: "🇦🇺", age: 23, image: "piastri.jpg" },
+      { number: 4,  firstName: "Lando", lastName: "NORRIS",  nationality: "British",    flag: "🇬🇧", age: 25, image: "lando-norris.png" },
+      { number: 81, firstName: "Oscar", lastName: "PIASTRI", nationality: "Australian", flag: "🇦🇺", age: 23, image: "oscar-piastri.png" },
     ],
   },
   {
@@ -63,8 +63,8 @@ export const teams: TeamData[] = [
     logoImage: "astonmartin.png",
     carImage: "astonmartin.png",
     drivers: [
-      { number: 14, firstName: "Fernando", lastName: "ALONSO", nationality: "Spanish",  flag: "🇪🇸", age: 43, image: "alonso.jpg" },
-      { number: 18, firstName: "Lance",    lastName: "STROLL", nationality: "Canadian", flag: "🇨🇦", age: 26, image: "stroll.jpg" },
+      { number: 14, firstName: "Fernando", lastName: "ALONSO", nationality: "Spanish",  flag: "🇪🇸", age: 43, image: "fernando-alonso.png" },
+      { number: 18, firstName: "Lance",    lastName: "STROLL", nationality: "Canadian", flag: "🇨🇦", age: 26, image: "lance-stroll.png" },
     ],
   },
   {
@@ -73,8 +73,8 @@ export const teams: TeamData[] = [
     logoImage: "alpine.png",
     carImage: "alpine.png",
     drivers: [
-      { number: 10, firstName: "Pierre", lastName: "GASLY",  nationality: "French",     flag: "🇫🇷", age: 29, image: "gasly.jpg" },
-      { number: 7,  firstName: "Jack",   lastName: "DOOHAN", nationality: "Australian", flag: "🇦🇺", age: 22, image: "doohan.jpg" },
+      { number: 10, firstName: "Pierre", lastName: "GASLY",  nationality: "French",     flag: "🇫🇷", age: 29, image: "pierre-gasly.png" },
+      { number: 7,  firstName: "Jack",   lastName: "DOOHAN", nationality: "Australian", flag: "🇦🇺", age: 22, image: "jack-doohan.png" },
     ],
   },
   {
@@ -83,8 +83,8 @@ export const teams: TeamData[] = [
     logoImage: "williams.png",
     carImage: "williams.png",
     drivers: [
-      { number: 23, firstName: "Alex",   lastName: "ALBON", nationality: "Thai",    flag: "🇹🇭", age: 29, image: "albon.jpg" },
-      { number: 55, firstName: "Carlos", lastName: "SAINZ", nationality: "Spanish", flag: "🇪🇸", age: 30, image: "sainz.jpg" },
+      { number: 23, firstName: "Alex",   lastName: "ALBON", nationality: "Thai",    flag: "🇹🇭", age: 29, image: "alex-albon.png" },
+      { number: 55, firstName: "Carlos", lastName: "SAINZ", nationality: "Spanish", flag: "🇪🇸", age: 30, image: "carlos-sainz.png" },
     ],
   },
   {
@@ -93,8 +93,8 @@ export const teams: TeamData[] = [
     logoImage: "haas.png",
     carImage: "haas.png",
     drivers: [
-      { number: 31, firstName: "Esteban", lastName: "OCON",    nationality: "French",  flag: "🇫🇷", age: 28, image: "ocon.jpg" },
-      { number: 87, firstName: "Oliver",  lastName: "BEARMAN", nationality: "British", flag: "🇬🇧", age: 19, image: "bearman.jpg" },
+      { number: 31, firstName: "Esteban", lastName: "OCON",    nationality: "French",  flag: "🇫🇷", age: 28, image: "esteban-ocon.png" },
+      { number: 87, firstName: "Oliver",  lastName: "BEARMAN", nationality: "British", flag: "🇬🇧", age: 19, image: "oliver-bearman.png" },
     ],
   },
   {
@@ -103,8 +103,8 @@ export const teams: TeamData[] = [
     logoImage: "racingbulls.png",
     carImage: "racingbulls.png",
     drivers: [
-      { number: 22, firstName: "Yuki",  lastName: "TSUNODA", nationality: "Japanese", flag: "🇯🇵", age: 24, image: "tsunoda.jpg" },
-      { number: 6,  firstName: "Isack", lastName: "HADJAR",  nationality: "French",   flag: "🇫🇷", age: 20, image: "hadjar.jpg" },
+      { number: 22, firstName: "Yuki",  lastName: "TSUNODA", nationality: "Japanese", flag: "🇯🇵", age: 24, image: "yuki-tsunoda.png" },
+      { number: 6,  firstName: "Isack", lastName: "HADJAR",  nationality: "French",   flag: "🇫🇷", age: 20, image: "isack-hadjar.png" },
     ],
   },
   {
@@ -113,8 +113,8 @@ export const teams: TeamData[] = [
     logoImage: "sauber.png",
     carImage: "sauber.png",
     drivers: [
-      { number: 27, firstName: "Nico",    lastName: "HÜLKENBERG", nationality: "German",    flag: "🇩🇪", age: 37, image: "hulkenberg.jpg" },
-      { number: 5,  firstName: "Gabriel", lastName: "BORTOLETO",  nationality: "Brazilian", flag: "🇧🇷", age: 20, image: "bortoleto.jpg" },
+      { number: 27, firstName: "Nico",    lastName: "HÜLKENBERG", nationality: "German",    flag: "🇩🇪", age: 37, image: "nico-hulkenberg.png" },
+      { number: 5,  firstName: "Gabriel", lastName: "BORTOLETO",  nationality: "Brazilian", flag: "🇧🇷", age: 20, image: "gabriel-bortoleto.png" },
     ],
   },
 ];
